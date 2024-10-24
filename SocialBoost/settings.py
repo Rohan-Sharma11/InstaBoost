94490d8c8ec2ebc0201c53be69762007c92b2b52
Rohan Sharma <rohansh3535@gmail.com>
2024-10-25 01:44:18 +0530

    updated ui

--- SocialBoost/settings.py
+++ SocialBoost/settings.py
@@ -132,4 +132,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'socialboooost1@gmail.com'
-EMAIL_HOST_PASSWORD = 'bqyh mizg vufo mgqp'
+EMAIL_HOST_PASSWORD = 'your_pass'
